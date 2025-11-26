9f94ccafc0b45b46c4a2ffaf99b9715cb80768b3
zhenghaoyang24 <zhenghaoyang24@foxmail.com>
2025-11-26 14:34:10 +0800

    fix: export error

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // VuePress 插件入口文件
-const { resolve } = require('path')
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
 
-module.exports = (options = {}, ctx) => {
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+export default (options = {}, ctx) => {
   return {
     name: 'vuepress-plume-music-widget',
     
